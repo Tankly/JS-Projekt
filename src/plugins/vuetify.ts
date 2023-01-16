5c3f8bcf12f6a8ccb5035ec66175ac8ef31bfc3b
Tankly <45769019+Tankly@users.noreply.github.com>
2023-01-16 20:09:59 +0100

    struktura plików sasowych, podmiana base kolorów na te z figmy,

--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -1,9 +1,34 @@
 // Styles
 import '@mdi/font/css/materialdesignicons.css'
-import 'vuetify/styles'
+import '@/assets/index.scss'
 
 // Vuetify
 import { createVuetify } from 'vuetify'
+import type { ThemeDefinition } from 'vuetify'
 
-export default createVuetify()
 // https://vuetifyjs.com/en/introduction/why-vuetify/#feature-guides
+
+const lightTheme: ThemeDefinition = {
+  dark: false,
+  colors: {
+    primary: '#5856D6',
+    secondary: '#FFFFFF',
+    background: '#F8F8F8',
+    surface: '#EFEFF4',
+    error: '#F43426',
+    info: '#007AFF',
+    success: '#4CD964',
+    warning: '#FFE200',
+    accent_dark_green: '#047C52',
+    accent_orange: '#FF9F0A',
+  },
+}
+
+export default createVuetify({
+  theme: {
+    defaultTheme: 'lightTheme',
+    themes: {
+      lightTheme,
+    },
+  },
+})
