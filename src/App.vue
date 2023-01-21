39a8331bb16dd100a9333afdd42e53fa79a68f43
Tankly <45769019+Tankly@users.noreply.github.com>
2023-01-21 16:55:45 +0100

    dodanie głównego layout i importowanie go do routera, dodanie plików logo, utilsy na flexa,

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,22 @@
 <template>
   <v-app>
-    <v-main>
-      <router-view />
-    </v-main>
+    <component :is="$route.meta.layout || 'mainLayout'">
+      <v-main>
+        <router-view />
+      </v-main>
+    </component>
   </v-app>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
+import mainLayout from '@/layouts/main.vue'
 
 export default defineComponent({
   name: 'App',
-
+  components: {
+    mainLayout,
+  },
   data() {
     return {
       //
---
+++ src/layouts/main.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-app-bar>
+    <v-app-bar-title class="u-flex__grow--3">Lista zakupów </v-app-bar-title>
+    <img :src="`${publicPath}img/logo64.png`" />
+  </v-app-bar>
+  <v-navigation-drawer>drawer</v-navigation-drawer>
+  <slot></slot>
+  <v-bottom-navigation>bot nav</v-bottom-navigation>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  data() {
+    return {
+      publicPath: import.meta.env.BASE_URL,
+    }
+  },
+})
+</script>
