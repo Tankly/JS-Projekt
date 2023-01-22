2b522aa68baa304f93f84684d6278461c618e753
Tankly <45769019+Tankly@users.noreply.github.com>
2023-01-22 16:06:29 +0100

    code split na Top nava przeniesienie scss od flexów do objects

---
+++ src/components/layout/organisms/TheTopNav.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-app-bar id="app-bar">
+    <v-app-bar-title class="u-flex__grow--3">
+      {{ $route.name }}
+    </v-app-bar-title>
+    <img :src="`${publicPath}img/logo-64x64.png`" />
+  </v-app-bar>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  data() {
+    return {
+      publicPath: import.meta.env.BASE_URL,
+    }
+  },
+})
+</script>
--- src/layouts/main.vue
+++ src/layouts/main.vue
@@ -1,10 +1,5 @@
 <template>
-  <v-app-bar id="app-bar">
-    <v-app-bar-title class="u-flex__grow--3">
-      {{ $route.name }}
-    </v-app-bar-title>
-    <img :src="`${publicPath}img/logo-64x64.png`" />
-  </v-app-bar>
+  <TheTopNav />
   <v-navigation-drawer>drawer</v-navigation-drawer>
   <slot></slot>
   <TheBottomNav />
@@ -12,14 +7,10 @@
 
 <script lang="ts">
 import TheBottomNav from '@/components/layout/organisms/TheBottomNav.vue'
+import TheTopNav from '@/components/layout/organisms/TheTopNav.vue'
 import { defineComponent } from 'vue'
 
 export default defineComponent({
-  data() {
-    return {
-      publicPath: import.meta.env.BASE_URL,
-    }
-  },
-  components: { TheBottomNav },
+  components: { TheBottomNav, TheTopNav },
 })
 </script>
