7c39c0b41305c04aa60bd0d0f6ff6a53b24bf12d
Tankly <45769019+Tankly@users.noreply.github.com>
2023-01-23 19:17:57 +0100

    początki featurea Lists, dodanie adaptera do mocków list, interface do list itemu i całej listy, service do shopping list

---
+++ src/components/modules/shopping/organisms/ShoppingList.vue
@@ -0,0 +1,42 @@
+<template>
+  <v-list-item
+    :key="data.name"
+    :title="data.name"
+    rounded
+    class="c-shopping-list-item"
+  >
+    <template v-slot:prepend>
+      <v-avatar :color="data.color">
+        <v-icon color="secondary">{{ data.icon }}</v-icon>
+      </v-avatar>
+    </template>
+
+    <template v-slot:append>
+      <v-btn
+        color="grey-lighten-1"
+        icon="mdi-information"
+        variant="text"
+      ></v-btn>
+    </template>
+  </v-list-item>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  props: {
+    data: {
+      type: Object,
+      required: true,
+    },
+  },
+})
+</script>
+
+<style scoped lang="scss">
+.c-shopping-list-item {
+  background-color: #e5e5e5;
+  width: 90%;
+}
+</style>
